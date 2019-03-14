c90374f398e13194c65356c84ef4b57cf1d34b65
shrutiAgg <shrutiag88@gmail.com>
2019-03-14 17:31:24 -0500

    Updated shcema and data.sql files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into tickets(id, price , seatNo, status) values(1,'20','Available','1A');
-insert into tickets(id, price , seatNo, status) values(2,'40','Available','2A');
-insert into tickets(id, price , seatNo, status) values(3,'30','Available','3A');
+insert into tickets(id, price ,status, seatno) values(1,'20','Available','1A');
+insert into tickets(id, price ,status, seatno) values(2,'40','Available','2A');
+insert into tickets(id, price ,status, seatno) values(3,'30','Available','3A');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE tickets
 (
  id numeric Not NULL,
  price  numeric NOT NULL ,
- seatNo varchar(20) NOT NULL,
+ seatno varchar(20) NOT NULL,
  status varchar(20) NOT NULL,
  PRIMARY KEY (id)
 );
